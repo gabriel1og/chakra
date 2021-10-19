77dcd2d8eafacf161953603ec2b1ae974e6827cb
Gabriel Gomide <gogomide.go@gmail.com>
2021-10-18 21:16:42 -0300

    att login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,45 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { Button, useColorMode, useColorModeValue, Input, Flex, Spacer, Heading } from "@chakra-ui/react";
+import { MoonIcon } from '@chakra-ui/icons'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const { toggleColorMode } = useColorMode()
+  const formBackground = useColorModeValue("gray.100", "gray.700")
+
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background={formBackground} p={12} rounded={10}>
+        <Heading
+          mb={8}
+          display="flex"
+          justifyContent="center">
+          Log In
+        </Heading>
+
+        <Input
+          placeholder="gabrielog@gmail.com"
+          variant="filled"
+          mb={3}
+          type="email" />
+
+        <Input
+          placeholder="********"
+          variant="filled"
+          mb={6}
+          type="password" />
+
+        <Button
+          mb={6}
+          colorScheme="teal">
+          Log In
+        </Button>
+
+        <Button onClick={toggleColorMode}>
+          <MoonIcon w={7} h={7} color="dark" />
+        </Button>
+      
+      </Flex>
+    </Flex>
+  )
+}
 
 export default IndexPage
